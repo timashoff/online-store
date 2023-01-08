87e9187e8c72c182e3b7037b5c4db2eea5ec5042
Roman Timashoff <83927854+timashoff@users.noreply.github.com>
2023-01-09 03:46:19 +0600

    feat: start main page component

---
+++ src/pages/main/main.ts
@@ -0,0 +1,6 @@
+
+import { createHTMLTemplate } from '../../utils/createHTMLTemplate';
+import './main.css';
+import mainHTML from './main.html';
+
+export const main = () => createHTMLTemplate(mainHTML);
