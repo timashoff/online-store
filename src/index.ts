97c2e2fd1e31f7cdbc8d16f6df40dc609413989c
Roman Timashoff <83927854+timashoff@users.noreply.github.com>
2023-01-09 01:19:17 +0600

    feat: implement footer components

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { header } from './components/header';
+import { footer } from './components/footer';
 import { addElement } from './utils/addElement';
 import './style.css';
 
 const HEADER = document.querySelector('.header');
+const FOOTER = document.querySelector('.footer');
 addElement(HEADER, header);
+addElement(FOOTER, footer);
