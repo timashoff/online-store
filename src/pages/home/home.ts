db23bcdb28350bf834a859cb1d16936d4ec30f9f
Roman Timashoff <83927854+timashoff@users.noreply.github.com>
2023-01-09 04:41:36 +0600

    feat: implement home page

---
+++ src/pages/home/home.ts
@@ -0,0 +1,6 @@
+
+import { createHTMLTemplate } from '../../utils/createHTMLTemplate';
+import './home.css';
+import mainHTML from './home.html';
+
+export const home = () => createHTMLTemplate(mainHTML);
--- src/pages/main/main.ts
+++
@@ -1,6 +0,0 @@
-
-import { createHTMLTemplate } from '../../utils/createHTMLTemplate';
-import './main.css';
-import mainHTML from './main.html';
-
-export const main = () => createHTMLTemplate(mainHTML);
