107255195faabf6a0cb6c3d19d23255ab2e594e5
Roman Timashoff <83927854+timashoff@users.noreply.github.com>
2023-01-10 03:31:13 +0600

    feat: initial implement product page

---
+++ src/pages/product/helpers/zoom-image.ts
@@ -0,0 +1,16 @@
+// const productImages = document.querySelectorAll('.product-image');
+
+// productImages.forEach((image) => {
+//   image.addEventListener('click', function () {
+//     // Create and append product image zoom element
+//     const productImageZoom = document.createElement('div');
+//     productImageZoom.classList.add('product-image-zoom');
+//     productImageZoom.innerHTML = `<img src="${this.dataset.zoom}">`;
+//     document.body.appendChild(productImageZoom);
+
+//     // Add event listener to close product image zoom
+//     productImageZoom.addEventListener('click', function () {
+//       this.remove();
+//     });
+//   });
+// });
---
+++ src/pages/product/product.ts
@@ -0,0 +1,6 @@
+
+import { createHTMLTemplate } from '../../utils/createHTMLTemplate';
+import './product.css';
+import productHTML from './product.html';
+
+export const product = () => createHTMLTemplate(productHTML);
