168e0b6294afe99e59cf8a2d0b1c115eca18779b
amir <amikamaymun@gmail.com>
2023-01-09 17:14:45 +0500

    add: path validation added

--- src/utils/router.ts
+++ src/utils/router.ts
@@ -1,6 +1,7 @@
 
 import { cart } from '../pages/cart';
 import { home } from '../pages/home/home';
+import { notFound } from '../pages/404/404';
 import { getHTMLElement } from './getHTMLElement';
 
 const root = getHTMLElement(document, '#root');
@@ -15,13 +16,21 @@ const routes = [
     path: '/cart',
   },
   { path: '/product/id1' },
+  {
+    page: notFound(),
+    path: '/404',
+  },
 ];
 
 export function render() {
-  const route = routes.find(r => r.path === window.location.pathname);
-  const page = route?.page;
-  if (page instanceof DocumentFragment) {
-    root.innerHTML = '';
-    root.appendChild(page);
+  if (routes.find(r => r.path === window.location.pathname)) {
+    const route = routes.find(r => r.path === window.location.pathname);
+    const page = route?.page;
+    if (page instanceof DocumentFragment) {
+      root.innerHTML = '';
+      root.appendChild(page);
+    }
+  } else {
+    window.location.pathname = '/404';
   }
 }
